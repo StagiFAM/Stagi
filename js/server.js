90923832be62567c00e1fa28c4ae4e1944ca5659
Luiz-Daniel-Oliveira <luiz.oliveira@ldanieloli.com>
2023-08-27 11:51:16 -0300

    Upload teste

--- js/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => 
-    console.log('Servidor iniciado na porta %d',PORT));
